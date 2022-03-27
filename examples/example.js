bcf5792c17b443dd6a2d17a7eae121478c0a7c54
Brandon <19891236+brandoncaulfield@users.noreply.github.com>
2022-03-27 14:04:51 +0100

    update examples

--- examples/example.js
+++ examples/example.js
@@ -5,31 +5,34 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-const ioc_library_example_1_1 = require("ioc-library-example-1");
-class Engine {
-    constructor() {
+exports.__esModule = true;
+var ioc_library_example_1_1 = require("ioc-library-example-1");
+var Engine = /** @class */ (function () {
+    function Engine() {
         this.engineCapacity = "2.0L";
     }
-    getEngineCapacity() {
+    Engine.prototype.getEngineCapacity = function () {
         return this.engineCapacity;
-    }
-}
-class Wheels {
-    constructor() {
+    };
+    return Engine;
+}());
+var Wheels = /** @class */ (function () {
+    function Wheels() {
         this.wheelCount = 4;
     }
-    getWheels() {
+    Wheels.prototype.getWheels = function () {
         return this.wheelCount;
-    }
-}
-let Car = class Car {
-    constructor(model) {
+    };
+    return Wheels;
+}());
+var Car = /** @class */ (function () {
+    function Car(model) {
         this.model = model;
     }
-};
-Car = __decorate([
-    ioc_library_example_1_1.Inject([Engine, new Wheels()])
-], Car);
-const mercedes = new Car("mercedes");
+    Car = __decorate([
+        ioc_library_example_1_1.Inject([Engine, new Wheels()])
+    ], Car);
+    return Car;
+}());
+var mercedes = new Car("mercedes");
 console.log(mercedes.model, mercedes.Engine.engineCapacity, mercedes.Wheels.wheelCount);
