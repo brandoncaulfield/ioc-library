bfb30d3b20254859a4d9b253b651e2a3db2352b5
Brandon <19891236+brandoncaulfield@users.noreply.github.com>
2022-03-28 09:02:53 +0100

    tidy comments

--- src/index.ts
+++ src/index.ts
@@ -13,13 +13,12 @@
 export function Inject(modules: any) {
   return function (target: any) {
     modules.forEach((module: any) => {
-      // Ifit's an instanitated class object just add it as a dependency
-      // console.log(`${module}: ${typeof module}`);
+      // If it's an instanitated class object just add it as a dependency
       if (typeof module === "object") {
         debugger;
         target.prototype[module.constructor.name] = module;
-        // If the class is not instantiated yet, intantiate it before adding it as a dependency
       } else if (typeof module === "function") {
+        // If the class is not instantiated yet, intantiate it before adding it as a dependency
         target.prototype[module.name] = new module();
       }
     });
