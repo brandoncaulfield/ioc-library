750e37ceb915746bad5c93f7012176e2ba5564ba
Brandon <19891236+brandoncaulfield@users.noreply.github.com>
2022-03-28 09:24:34 +0100

    updated comments, removed debugger

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,5 @@
 /**
- *
- * TODO:
- * 1.
- */
-
-/**
- * Inject function that adds new classes or objects to a class's prototype (as dependencies)
- * @param modules
- * @returns
+ * Returns a function that adds classes/ or objects to a class's prototype.
  */
 
 export function Inject(modules: any) {
@@ -15,7 +7,6 @@ export function Inject(modules: any) {
     modules.forEach((module: any) => {
       // If it's an instanitated class object just add it as a dependency
       if (typeof module === "object") {
-        debugger;
         target.prototype[module.constructor.name] = module;
       } else if (typeof module === "function") {
         // If the class is not instantiated yet, intantiate it before adding it as a dependency
