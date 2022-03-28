c3bfb7669369e1f31120dee6987ea05574f1e239
Brandon <19891236+brandoncaulfield@users.noreply.github.com>
2022-03-28 12:14:03 +0100

    update README

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,15 @@
  */
 
 export function Inject(modules: {}[]) {
+  /**
+   * TODO: target param is currently untyped but will need to be properly typed
+   * to ensure only relevant objects are accepted and communicated.
+   */
   return function (target: any) {
+    /**
+     * TODO: Add relevant type(s) and setup tests to ensure "module" param is
+     * correct for prototype assignment.
+     */
     modules.forEach((module: any) => {
       if (typeof module === "object") {
         // If it's an instanitated class object just add it as a dependency
