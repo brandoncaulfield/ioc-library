d6d54a0e96b7bea05e39a21c7215e32f475fdd55
Brandon <19891236+brandoncaulfield@users.noreply.github.com>
2022-04-05 08:36:24 +0100

    TODO's

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 /**
+ * TODO:
+ * 1. FIX (typescript) test script
+ * 2. ADD Circular dependency detection
+ * 3. ADD Singleton functionality
+ * 4. ADD decent Error handling
+ */
+
+/**
  * Returns a function that adds classes/ or objects to a class's prototype.
  */
 
@@ -13,12 +21,18 @@ export function Inject(modules: {}[]) {
      * correct for prototype assignment.
      */
     modules.forEach((module: any) => {
-      if (typeof module === "object") {
-        // If it's an instanitated class object just add it as a dependency
-        target.prototype[module.constructor.name] = module;
-      } else if (typeof module === "function") {
-        // If the class is not instantiated yet, intantiate it before adding it as a dependency
-        target.prototype[module.name] = new module();
+      try {
+        if (typeof module === "object") {
+          // If it's an instanitated class object just add it as a dependency
+          target.prototype[module.constructor.name] = module;
+        } else if (typeof module === "function") {
+          // If the class is not instantiated yet, intantiate it before adding it as a dependency
+          target.prototype[module.name] = new module();
+        }
+      } catch (err) {
+        throw new Error(
+          `The following error: ${err} occured at Module: ${module}`
+        );
       }
     });
   };
